9843b4fbb51060a5c675613d195f666fd268ac41
Per Kristian Kummermo <pkkummermo@lab5e.com>
2021-10-24 19:55:18 +0200

    test(index): add expose test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -16,4 +16,12 @@ describe("Index", () => {
   it("should expose validation", () => {
     expect(index["validation"]).toBeDefined();
   });
+
+  it("should expose vue utils", () => {
+    expect(index["vueUtil"]).toBeDefined();
+  });
+
+  it("should expose vuetify utils", () => {
+    expect(index["vuetifyUtil"]).toBeDefined();
+  });
 });
