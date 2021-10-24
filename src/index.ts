bc98c1a6fefead3cbb77428ae1802cd66b602f4e
Per Kristian Kummermo <pkkummermo@lab5e.com>
2021-10-24 19:41:31 +0200

    feat(index): export vue and vuetify utils

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import * as validation from "./validation";
+import * as vueUtil from "./vue/index";
+import * as vuetifyUtil from "./vuetify/index";
+
 export * from "./sleep";
 export * from "./copy";
 export * from "./clone";
-export { validation };
+
+export { validation, vueUtil, vuetifyUtil };
